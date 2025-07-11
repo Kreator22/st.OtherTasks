a18c1a2578bac20584acc48e2f4cdb1a42111ffb
Kreator2 <kulakov_aa@list.ru>
2025-07-11 23:29:03 +1200

    Broken Keyboard

---
+++ BrokenKeyboard/BrokenKeyboard/Printer.cs
@@ -0,0 +1,146 @@
+﻿using System.Text;
+
+namespace BrokenKeyboard
+{
+    public static class Printer
+    {
+        /// <summary>
+        /// Сложность по времени - в худшем случае О(n^2) 
+        /// (для каждого символа придётся проверить все остальные до начала строки),
+        /// в лучшем случае O(n)
+        /// (для каждой b или B символ для удаления - предыдущий)
+        /// Сложность по памяти - O(n)
+        /// </summary>
+        public static string Print1(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "";
+
+            char[] chars = input.ToArray();
+
+            for ((int i, int j) = (0, 0); i < chars.Length; i++)
+            {
+                if (chars[i] == 'b')
+                {
+                    j = i;
+                    while (j > 0 && (char.IsUpper(chars[--j]) || chars[j] == ' ')) ;      
+                    chars[j] = ' ';
+                    chars[i] = ' ';
+                    continue;
+                }
+
+                if (chars[i] == 'B')
+                {
+                    j = i;
+                    while (j > 0 && (char.IsLower(chars[--j]) || chars[j] == ' ')) ;
+                    chars[j] = ' ';
+                    chars[i] = ' ';
+                    continue;
+                }
+            }
+
+            return new string(chars.Where(c => c != ' ').ToArray());
+        }
+
+        /// <summary>
+        /// Сложность по времени - O(n)
+        /// Сложность по памяти - O(n)
+        /// </summary>
+        public static string Print2(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "";
+
+            Stack<int> lowPos = new();
+            Stack<int> upPos = new();
+
+            char[] chars = input.ToArray();
+
+            for(int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == 'b')
+                {
+                    if (lowPos.Count > 0)
+                        chars[lowPos.Pop()] = ' ';
+                    chars[i] = ' ';
+                    continue;
+                }
+                
+                if (chars[i] == 'B')
+                {
+                    if (upPos.Count > 0)
+                        chars[upPos.Pop()] = ' ';
+                    chars[i] = ' ';
+                    continue;
+                }
+
+                else if (char.IsLower(chars[i]))
+                    lowPos.Push(i);
+                else
+                    upPos.Push(i);
+            }
+
+            return new string(chars.Where(c => c != ' ').ToArray());
+        }
+
+        /// <summary>
+        /// Решение отсюда:
+        /// https://gist.github.com/DimsFromDergachy/28e501cb57557eaf070a67eac22e5e8f
+        /// </summary>
+        public static string Print3(string a)
+        {
+            // Stacks keep good indexes
+            var lowers = new Stack<(int, char chr)>(a.Count());
+            var uppers = new Stack<(int, char chr)>(a.Count());
+
+            for (int i = 0; i < a.Count(); i++)
+            {
+                var ch = a[i];
+                switch (ch)
+                {
+                    case 'b':
+                        lowers.TryPop(out _); break;
+                    case 'B':
+                        uppers.TryPop(out _); break;
+                    case var o when char.IsLower(o):
+                        lowers.Push((i, ch)); break;
+                    case var o when char.IsUpper(o):
+                        uppers.Push((i, ch)); break;
+                    default:
+                        throw new NotSupportedException($"Unsupported character: '{ch}'");
+                }
+            }
+
+            var result = new Stack<char>(a.Length);
+
+            while (lowers.Any() || uppers.Any())
+            {
+                if (!lowers.Any())
+                {
+                    result.Push(uppers.Pop().chr); continue;
+                }
+                if (!uppers.Any())
+                {
+                    result.Push(lowers.Pop().chr); continue;
+                }
+
+                (int i1, _) = lowers.Peek();
+                (int i2, _) = uppers.Peek();
+
+                if (i1 > i2)
+                {
+                    result.Push(lowers.Pop().chr); continue;
+                }
+                if (i1 < i2)
+                {
+                    result.Push(uppers.Pop().chr); continue;
+                }
+
+                throw new NotSupportedException("Broken invariant");
+            }
+
+            return new string(result.ToArray());
+        }
+
+    }
+}
---
+++ BrokenKeyboard/Tests/Test1.cs
@@ -0,0 +1,85 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Func<string, string> funcs;
+            funcs = BrokenKeyboard.Printer.Print1;
+            funcs += BrokenKeyboard.Printer.Print2;
+            funcs += BrokenKeyboard.Printer.Print3;
+
+            Tester(funcs);
+
+            int n = 100000;
+            string timeTest = StringGenerator(n);
+
+            Console.WriteLine("Сравнение скорости на произвольной строке");
+            Console.WriteLine("b и B встречаются так же часто, как и остальные символы");
+            TimeTester(funcs, timeTest);
+
+            StringBuilder sb = new();
+            for (int i = 0; i < n / 2; i++)
+                sb.Append('b');
+            string b = sb.ToString();
+
+            Console.WriteLine();
+            Console.WriteLine("Сравнение скорости для плохого случая");
+            Console.WriteLine("Треть от всех символов - b, почти все в конце строки");
+            TimeTester(funcs, string.Concat(timeTest, b));
+        }
+
+       static string StringGenerator(int length)
+        {
+            StringBuilder sb = new(length);
+
+            for(; length > 0; length--)
+            {
+                int n = Random.Shared.Next(1, 53) + 64;
+                char c = n <= 26 + 64 ? (char)n : (char)(n + 6);
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+
+        static void Tester(Func<string, string> func)
+        {
+            string source = "bBqwertyQWERTYbBbB";
+            string expected = "qwerQWER";
+            string actual;
+
+            foreach(Func<string, string> f in func.GetInvocationList())
+            {
+                actual = f(source);
+                Assert.AreEqual(expected, actual);
+            }
+
+            source = "bBqwertybbBBQWERTYbBbB";
+            expected = "qwQWER";
+            foreach (Func<string, string> f in func.GetInvocationList())
+            {
+                actual = f(source);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        static void TimeTester(Func<string, string> func, string argument)
+        {
+            Stopwatch sw = new();
+
+            foreach(Func<string,string> f in func.GetInvocationList())
+            {
+                sw.Restart();
+                f(argument);
+                sw.Stop();
+                Console.WriteLine(sw.Elapsed + " - " + f.Method.Name);
+            } 
+        }
+    }
+}
