ef97e5ed5c6941de4d34b4df1b32577f66f5f91d
Kreator2 <kulakov_aa@list.ru>
2025-07-27 13:26:12 +1200

    Фильтр кандидатов

---
+++ CandidatesFilter/Filter/Filter.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Filter
+{
+    public static class Filter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// Хотя по заданию ожидаются строки, сигнатура метода взята напрямую
+        /// из компилятора hh.ru в котором проверяется решение, т.е. на вход там 
+        /// отдают Dictionary
+        /// </remarks>
+        public static List<string> SelectCandidates(Dictionary<string, int[]> candidates)
+        {
+            var result = candidates
+                .Select(c => (
+                    name : c.Key, 
+                    average : Double.Round( (double)c.Value.Sum() / (double)c.Value.Length, 1)))
+                .Where(c => c.average >= 5.0)
+                .OrderBy(c => -c.average)
+                .ThenBy(c => c.name)
+                .Select(c => c.name + "," + c.average.ToString("F1", CultureInfo.InvariantCulture))
+                .ToList();
+
+            if (!result.Any())
+                result.Add("Никто");
+
+            return result;
+        }
+    }
+}
---
+++ CandidatesFilter/FilterTest/Test1.cs
@@ -0,0 +1,29 @@
+﻿using static Filter.Filter;
+
+namespace FilterTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var candidates = new Dictionary<string, int[]>();
+            candidates.Add("Ivanov", [5, 6, 7, 8]);
+            candidates.Add("Lisii", [9, 8, 10, 9]);
+            candidates.Add("Sokolova", [5, 6, 8, 5]);
+            candidates.Add("Tritonov", [7, 2, 3, 4]);
+            candidates.Add("Chernov", [8, 8, 8, 8]);
+            candidates.Add("Svetova", [4, 5, 3, 6]);
+            candidates.Add("Zayats", [5, 5, 5, 5]);
+            candidates.Add("Rezhik", [6, 6, 6, 6]);
+            candidates.Add("Test", [10, 10, 9]);
+
+            List<string> actual = new();
+            actual = SelectCandidates(candidates);
+
+            foreach (var can in actual)
+                Console.WriteLine(can);
+        }
+    }
+}
